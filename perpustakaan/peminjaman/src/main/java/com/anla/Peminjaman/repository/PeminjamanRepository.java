43441451ec87a0bca0c8c1c8d787f1ece8d3f3a1
itsanla <anlaharpanda@gmail.com>
2025-12-30 10:04:07 +0700

    refactor: remove JPA repositories and use environment variables for configuration

--- perpustakaan/peminjaman/src/main/java/com/anla/Peminjaman/repository/PeminjamanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anla.Peminjaman.repository;
-
-import com.anla.Peminjaman.model.Peminjaman;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PeminjamanRepository extends JpaRepository<Peminjaman, Long> {
-}
--- perpustakaan/pengembalian/src/main/java/com/anla/Pengembalian/repository/PengembalianRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anla.Pengembalian.repository;
-
-import com.anla.Pengembalian.model.Pengembalian;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PengembalianRepository extends JpaRepository<Pengembalian, Long> {
-}
