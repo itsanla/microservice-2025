bf3d7be140c2c65bdd9f2790e0a5fca6e2b4d74b
itsanla <anlaharpanda@gmail.com>
2026-01-10 18:35:48 +0700

    ads

--- eureka/src/test/java/com/example/eureka/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/example/eureka/eureka/EurekaApplicationTests.java
@@ -1,10 +1,18 @@
 package com.example.eureka.eureka;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "eureka.client.enabled=false",
+    "eureka.server.enable-self-preservation=false"
+})
+@EnableAutoConfiguration(exclude = {
+    org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration.class
+})
 @ActiveProfiles("test")
 class EurekaApplicationTests {
 
