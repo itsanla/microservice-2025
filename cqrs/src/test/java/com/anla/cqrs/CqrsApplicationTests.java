32d81e6ed33f23bda6c2a872be604b3e99620c37
itsanla <anlaharpanda@gmail.com>
2026-01-10 18:34:45 +0700

    ff

--- cqrs/src/test/java/com/anla/cqrs/CqrsApplicationTests.java
+++ cqrs/src/test/java/com/anla/cqrs/CqrsApplicationTests.java
@@ -1,10 +1,12 @@
 package com.anla.cqrs;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(excludeAutoConfiguration = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ActiveProfiles("test")
 class CqrsApplicationTests {
 
