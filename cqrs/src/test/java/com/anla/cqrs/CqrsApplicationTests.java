8d7784e11d2d77961d99781221e3cf66c99dc731
itsanla <anlaharpanda@gmail.com>
2026-01-10 18:37:01 +0700

    adsd

--- cqrs/src/test/java/com/anla/cqrs/CqrsApplicationTests.java
+++ cqrs/src/test/java/com/anla/cqrs/CqrsApplicationTests.java
@@ -1,12 +1,14 @@
 package com.anla.cqrs;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(excludeAutoConfiguration = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ActiveProfiles("test")
 class CqrsApplicationTests {
 
