7bdffa1e64948bbbbabff9f051b829d0d109326e
itsanla <anlaharpanda@gmail.com>
2026-01-11 12:14:34 +0700

    dsa

--- cqrs/src/main/java/com/anla/cqrs/config/MultiDatabaseConfig.java
+++ cqrs/src/main/java/com/anla/cqrs/config/MultiDatabaseConfig.java
@@ -13,15 +13,21 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class MultiDatabaseConfig {
     
+    @Value("${spring.datasource.username:sa}")
+    private String h2Username;
+    
+    @Value("${spring.datasource.password:password}")
+    private String h2Password;
+    
     private final Map<String, DataSource> h2DataSources = new ConcurrentHashMap<>();
     
     public DataSource getH2DataSource(String serviceName) {
         return h2DataSources.computeIfAbsent(serviceName, name ->
             DataSourceBuilder.create()
                 .driverClassName("org.h2.Driver")
-                .url("jdbc:h2:file:./h2db/" + name + "_eventstore")
-                .username("sa")
-                .password("password")
+                .url("jdbc:h2:file:/data/h2/" + name + "_eventstore")
+                .username(h2Username)
+                .password(h2Password)
                 .build()
         );
     }
