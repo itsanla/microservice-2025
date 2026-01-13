113abf848fa9872b1c64dca406421fd224c4190c
itsanla <anlaharpanda@gmail.com>
2026-01-13 07:16:53 +0700

    Fix Kubernetes DNS discovery - hapus @LoadBalanced dari 5 service
    Masalah:
    - @LoadBalanced memaksa RestTemplate menggunakan Eureka untuk service discovery
    - Konflik dengan arsitektur K8s DNS (http://cqrs diselesaikan via CoreDNS)
    - Error: No instances available for cqrs karena fetch-registry=false
    
    Solusi:
    - Hapus @LoadBalanced dari RestTemplate bean di 5 service:
      1. anggota
      2. pengembalian
      3. buku
      4. pelanggan
      5. produk
    - RestTemplate sekarang gunakan standard DNS resolution (Kubernetes Service)
    - Eureka tetap aktif hanya untuk monitoring/pajangan
    
    Service discovery sekarang 100% via Kubernetes DNS

--- perpustakaan/anggota/src/main/java/com/anla/anggota/AnggotaApplication.java
+++ perpustakaan/anggota/src/main/java/com/anla/anggota/AnggotaApplication.java
@@ -3,7 +3,6 @@ package com.anla.anggota;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +15,6 @@ public class AnggotaApplication {
     }
     
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- perpustakaan/buku/src/main/java/com/anla/buku/BukuApplication.java
+++ perpustakaan/buku/src/main/java/com/anla/buku/BukuApplication.java
@@ -3,7 +3,6 @@ package com.anla.buku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +15,6 @@ public class BukuApplication {
     }
     
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
